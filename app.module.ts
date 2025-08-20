0d2f0aa4914bbceb8502136781019d567f64e33d
samikshabhujabal <samikshabhujabal@gmail.com>
2025-08-20 18:04:51 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { UpdateStudentComponent } from './components/update-student/update-student.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentListComponent,
+    AddStudentComponent,
+    UpdateStudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
